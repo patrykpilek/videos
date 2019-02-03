2a5d9338eef1e61507923ec766d0ebf5d4583e4a
Patryk Pilek <patryk.pilek@gmail.com>
2019-02-03 21:03:53 +0000

    add semantic-ui package, event handling form submittal.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    state = { term: '' };
+
+    onInputChange = event => {
+        this.setState({ term: event.target.value });
+    };
+
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        // ToDo: Make sure we call callback from parent component
+    };
+
     render() {
-        return <div>SearchBar</div>;
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label htmlFor="video_search">Video Search</label>
+                        <input type="text" id="video_search" value={ this.state.term } onChange={ this.onInputChange }/>
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
