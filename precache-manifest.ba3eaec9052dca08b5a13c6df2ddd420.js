b7b1df961223c64b899ad0c1901098e6f6ee23f8
Patryk Pilek <patryk.pilek@gmail.com>
2019-02-05 23:45:53 +0000

    Updates

---
+++ precache-manifest.ba3eaec9052dca08b5a13c6df2ddd420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb30758431df4dcb2543",
+    "url": "/videos/static/js/runtime~main.fb307584.js"
+  },
+  {
+    "revision": "15281ded9dc1cefc2fe6",
+    "url": "/videos/static/js/main.15281ded.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/videos/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "15281ded9dc1cefc2fe6",
+    "url": "/videos/static/css/main.3d27488d.chunk.css"
+  },
+  {
+    "revision": "daaacf279353639f625a1e49f18f59f9",
+    "url": "/videos/index.html"
+  }
+];
